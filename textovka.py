59d120cccd952a24865e209d4154dc7f800dbf4d
DavidValkoc <david.valkoc@t-systems.com>
2021-11-11 10:47:09 +0100

    del

--- textovka.py
+++ textovka.py
@@ -1,2 +1,3 @@
 #!/usr/bin/env python3
 
+#!/usr/bin/env python3#!/usr/bin/env python3#!/usr/bin/python3#!/usr/bin/env python3#!/usr/bin/env python3#!/usr/bin/env python3#!/usr/bin/env python3python3#!/usr/bin/env python3#!/usr/bin/env python3#!/usr/bin/env python3#!/usr/bin/env python3python3#!/usr/bin/env python3#!/usr/bin/env python3#!/usr/bin/env python3#!/usr/bin/env python3python3#!/usr/bin/env python3#!/usr/bin/env python3#!/usr/bin/env python3#!/usr/bin/env python3env python3#!/usr/bin/env python3#!/usr/bin/env python3
