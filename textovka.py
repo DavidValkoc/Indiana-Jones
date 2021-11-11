0287578bcad56c267b75428bffc7d7c84a3a488a
DavidValkoc <david.valkoc@t-systems.com>
2021-11-11 10:07:34 +0100

    154321534fff

--- textovka.py
+++ textovka.py
@@ -5,3 +5,8 @@ if __name__ =='__main__':
 
 if __name__ =='__main__':
     print('Indiana Jones and the Escape from the Dark Room')
+    if __name__ == '__main__':
+        print('Indiana Jones and the Escape from the Dark Room')
+
+    if __name__ == '__main__':
+        print('Indiana Jones and the Escape from the Dark Room')
