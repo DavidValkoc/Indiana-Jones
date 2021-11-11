51be4c3416cbe5c9cf93250fb06bf86ba2f4e694
DavidValkoc <david.valkoc@t-systems.com>
2021-11-11 13:23:16 +0100

    add room

--- textovka.py
+++ textovka.py
@@ -1,6 +1,21 @@
 #!/usr/bin/env python3
 import states
 
+
+def show_room(room):
+    print(f'Nachádzaš sa v miestnosti {room["name"]}.')
+    print(f'{room["description"]}')
+
+
+room = {
+    'name': 'dungeon',
+    'description': 'Nachádzaš sa v tmavej zatuchnutej miestnosti. Na kamenných stenách sa nenachádza žiadne okno, '
+                   'čo dáva tušiť, že si niekoľko metrov pod zemou. Žeby košický hrad? Aj to je možné, ti prebleslo '
+                   'hlavou.',
+    'items': [],
+    'exits': []
+}
+
 if __name__ == '__main__':
     # banner
     print(" ___           _ _                         _                       ")
@@ -11,29 +26,43 @@ if __name__ == '__main__':
     print("                       and his Great Escape                        ")
     print()
 
+    # rendering the dark room
+    show_room(room)
+
     # main loop
     game_state = states.PLAYING
     while game_state == states.PLAYING:
         # normalizing string
         line = input('> ').lower().strip()
 
+        # empty input
         if line == '':
             continue
 
+        # quit game
         elif line in ('koniec', 'quit', 'bye', 'q'):
             game_state = states.QUIT
 
+        # about game
         elif line in ('o hre', 'about', 'info', '?'):
-            print('(c)2021 created by mirek')
-            print('Ďalšie veľké dobrodružstvo Indiana Jonesa. Tentokrá zápasí s jazykom Python v tmavej miestnosti.')
+            print('(c)2021 created by Hemisfer')
+            print('Ďalšie veľké dobrodružstvo Indiana Jonesa. Tentokrát zápasí s jazykom Python v tmavej miestnosti.')
 
+        # show commands
         elif line in ('prikazy', 'commands', 'help', 'pomoc'):
             print('Zoznam príkazov v hre:')
             print('* koniec - ukončí rozohratú hru')
             print('* o hre - zobrazí informácie o hre')
             print('* prikazy - zobrazí príkazy, ktoré sa dajú použiť v hre')
+            print('* rozhliadni sa - vypíše opis miestnosti, v ktorej sa hráč práve nachádza')
 
+        # render room
+        elif line in ("rozhliadni sa", "look around", "kukaj het"):
+            show_room(room)
+
+        # unknown commands
         else:
             print('Taký príkaz nepoznám.')
 
-    print('(c)2021 by Hemisfer')
+    # game credits
+    print('(c)2021 by Hemisfer mocný programátor')
