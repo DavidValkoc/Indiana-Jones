c2e6410fcd252224979d39ab3f17b2dce84b74d9
DavidValkoc <david.valkoc@t-systems.com>
2021-11-11 10:18:13 +0100

    del

--- textovka.py
+++ textovka.py
@@ -1,12 +1,2 @@
 #!/usr/bin/env python3
 
-if __name__ =='__main__':
-    print('Indiana Jones and the Escape from the Dark Room')
-
-if __name__ =='__main__':
-    print('Indiana Jones and the Escape from the Dark Room')
-    if __name__ == '__main__':
-        print('Indiana Jones and the Escape from the Dark Room')
-
-    if __name__ == '__main__':
-        print('Indiana Jones and the Escape from the Dark Room')
